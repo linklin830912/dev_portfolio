261bb60a444c94f5eb27833dde01dd0374ada41a
Link Lin <linklin830912@gmail.com>
2024-08-27 16:51:58 +1000

    deploy

--- src/sections/PageSection.tsx
+++ src/sections/PageSection.tsx
@@ -8,7 +8,7 @@ function PageSection() {
     return (
         <div className="w-[100%] px-2 lg:px-0 overflow-hidden">            
             <Routes>
-                <Route path="/dev_portfolio/about" element={<AboutPage />} />
+                <Route path="/dev_portfolio" element={<AboutPage />} />
                 <Route path="/dev_portfolio/portfolio" element={ <PortfolioPage/> } />
                 <Route path="/dev_portfolio/resume" element={ <ResumePage/> } />
                 <Route path="/dev_portfolio/contact" element={ <ContactPage/> } />
