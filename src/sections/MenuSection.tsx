a6b155666a36ec7aa2a7507038821c95e4bf6c6c
Link Lin <linklin830912@gmail.com>
2024-08-27 16:59:25 +1000

    route

--- src/sections/MenuSection.tsx
+++ src/sections/MenuSection.tsx
@@ -7,10 +7,10 @@ function MenuSection() {
     return (
         <div className="flex flex-col md:pt-3 md:flex-row md:items-center justify-between h-[100%]">
             <div className="flex flex-col md:flex-row">
-                <MenuButton to="/" content={t('menu:about')} />
-                <MenuButton to="/portfolio"  content={t('menu:portfolio')} />
-                <MenuButton to="/resume" content={t('menu:resume')} />
-                <MenuButton to="/contact" content={t('menu:contact')} />
+                <MenuButton to="/dev_portfolio" content={t('menu:about')} />
+                <MenuButton to="/dev_portfolio/portfolio"  content={t('menu:portfolio')} />
+                <MenuButton to="/dev_portfolio/resume" content={t('menu:resume')} />
+                <MenuButton to="/dev_portfolio/contact" content={t('menu:contact')} />
             </div>
             <div className="text-right p-1 pb-32 text-h3 text-menu-font">{"▹"}</div>
             <div className="p-5">
