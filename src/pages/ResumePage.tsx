1e4431387f01dffade66100adb19a56948275fda
Link Lin <linklin830912@gmail.com>
2024-09-09 22:48:48 +1000

    drive renew

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -43,10 +43,10 @@ function ResumePage() {
                 <div className={style.spotlightRight}  style={{transform: `rotate(${-degree}deg)`}}></div>
             </div>            
             <a className={`w-[100%] max-w-[350px] ${styles.floatA} px-10 py-3 bg-content-font hover:bg-advanced text-menu-blue font-bold rounded-[100px] text-h3 whitespace-nowrap relative`}
-                href="https://drive.google.com/uc?export=download&id=1w4ekJ5cT9X578-BtMKZgd9ZBMoNKXqo7">                
+                href="https://drive.google.com/uc?export=download&id=1eFiIcnGcoJz1K6-FVq9YhtAhVNK1uHqw">                
                 <div className="flex flex-row justify-center items-center"><h2 className="mr-3">{t("resume")}</h2> <DownloadIcon /></div> 
             </a> 
         </div>
-    );
+    );//https://drive.google.com/file/d/1eFiIcnGcoJz1K6-FVq9YhtAhVNK1uHqw/view?usp=drive_link
  }
 export default ResumePage;
